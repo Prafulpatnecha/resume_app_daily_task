9def755bb94b36b03048272c5a6edbfa0e6a18b3
Praful Patnecha <prafulpatnecha@gmail.com>
2024-05-07 13:50:56 +0530

    Initial commit

---
+++ lib/HomeScreen/home/homepage.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:resume_app_daily_task/utils/routes.dart';
+class ResumeApp extends StatelessWidget {
+  const ResumeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.cyanAccent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: AppRoutes.routes,
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   systemOverlayStyle: const SystemUiOverlayStyle(
+      //     statusBarColor: Colors.cyanAccent
+      //   ),
+      // ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'HomeScreen/home/homepage.dart';
+void main()
+{
+  runApp(const ResumeApp());
+}
